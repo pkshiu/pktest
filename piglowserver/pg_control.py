76d49f6114ff62480bfa1d04aecef5723c2f9c3d
PK Shiu <pk@imperial-consulting.com>
2014-10-20 12:36:05 -0400

    interface to real rest server

--- piglowserver/pg_control.py
+++ piglowserver/pg_control.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import (Flask, url_for, redirect)
 from flask import (render_template, request)
 
@@ -5,14 +7,31 @@ from flask import (render_template, request)
 app = Flask(__name__)
 app.config.from_envvar('PGS_SETTINGS', silent=True)
 
+# zero entry is not used
+led_list = [{'led_id': i, 'brightness': 0} for i in range(0,20)]
+ARM_LED_LIST = map(tuple, ( [{'led_id': i, 'brightness': 0} for i in range(1,7)],
+                            [{'led_id': i, 'brightness': 0} for i in range(7,13)],
+                            [{'led_id': i, 'brightness': 0} for i in range(13,19)]))
 
-
-@app.route('/')
+@app.route('/', methods=['GET',])
 def show_control():
-    return render_template('control.html', led_list=led_list)
+    return render_template('control.html', led_list=led_list, arm_list=ARM_LED_LIST)
+
+@app.route('/set_leds', methods=['POST',])
+def set_leds():
+
+
+    ids = []
+    for i in range(1,19):
+        v = request.form.get('led_%d' % i, 0)
+        data = {'brightness': v}
+        r = requests.put('http://192.168.2.124:5000/leds/%d' % i, data=data)
+        print i,v
+
+    return redirect(url_for('show_control'))
 
 
 
 if __name__ == '__main__':
     print led_list
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=8000)
