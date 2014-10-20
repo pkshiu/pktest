58ce2c26be4fe271564a8c5171d745b4e2d1da31
PK Shiu <pk@imperial-consulting.com>
2014-10-20 16:25:10 -0400

    threading

--- piglowserver/pg_control.py
+++ piglowserver/pg_control.py
@@ -8,28 +8,30 @@ app = Flask(__name__)
 app.config.from_envvar('PGS_SETTINGS', silent=True)
 
 # zero entry is not used
-led_list = [{'led_id': i, 'brightness': 0} for i in range(0,20)]
-ARM_LED_LIST = map(tuple, ( [{'led_id': i, 'brightness': 0} for i in range(1,7)],
-                            [{'led_id': i, 'brightness': 0} for i in range(7,13)],
-                            [{'led_id': i, 'brightness': 0} for i in range(13,19)]))
+led_list = [{'led_id': i, 'brightness': 0} for i in range(0, 20)]
+ARM_LED_LIST = map(tuple, ([{'led_id': i, 'brightness': 0} for i in range(1, 7)],
+                           [{'led_id': i, 'brightness': 0} for i in range(7, 13)],
+                           [{'led_id': i, 'brightness': 0} for i in range(13, 19)]))
 
-@app.route('/', methods=['GET',])
+PG_SERVER = 'http://192.168.2.124:5000'
+PG_SERVER = 'http://localhost:5000'
+
+
+@app.route('/', methods=['GET', ])
 def show_control():
     return render_template('control.html', led_list=led_list, arm_list=ARM_LED_LIST)
 
-@app.route('/set_leds', methods=['POST',])
+
+@app.route('/set_leds', methods=['POST', ])
 def set_leds():
-
-
     ids = []
-    for i in range(1,19):
+    for i in range(1, 19):
         v = request.form.get('led_%d' % i, 0)
         data = {'brightness': v}
-        r = requests.put('http://192.168.2.124:5000/leds/%d' % i, data=data)
-        print i,v
+        r = requests.put('%s/leds/%d' % (PG_SERVER, i), data=data)
+        print i, v
 
     return redirect(url_for('show_control'))
-
 
 
 if __name__ == '__main__':
--- piglowserver/pg_rest_server.py
+++ piglowserver/pg_rest_server.py
@@ -1,8 +1,15 @@
+import threading
+import time
+
 from flask import (Flask, url_for, redirect)
 from flask import (render_template, request)
 from flask.ext.restful import (Resource, Api)
 
-from PyGlow import (PyGlow, ARM_LED_LIST)
+try:
+    from PyGlow import (PyGlow, ARM_LED_LIST)
+except ImportError:
+    from dummy_pyglow import (PyGlow, ARM_LED_LIST)
+
 from time import sleep
 
 pyglow = PyGlow()
@@ -12,20 +19,30 @@ app.config.from_envvar('PGS_SETTINGS', silent=True)
 api = Api(app)
 
 # zero entry is not used
-led_list = [{'led_id': i, 'brightness': 0} for i in range(0,20)]
+led_list = [{'led_id': i, 'brightness': 0} for i in range(0, 20)]
+
+# global lock
+lock = threading.Lock()
 
 
 # interface to the h/w layer
 def set_led(num, brightness):
-    led_list[num]['brightness'] = brightness
-    pyglow.led(num, brightness=brightness)
-#    pyglow.update_leds()
+    global lock
+
+    # do this one at a time
+    with lock:
+        led_list[num]['brightness'] = brightness
+        pyglow.led(num, brightness=brightness)
+        print 'sleeping...'
+        time.sleep(1)
+
 
 def set_arm(num, brightness):
-    for i in ARM_LED_LIST[num-1]:
+    for i in ARM_LED_LIST[num - 1]:
         led_list[i]['brightness'] = brightness
     pyglow.arm(num, brightness=brightness)
 #    pyglow.update_leds()
+
 
 class LedListAPI(Resource):
     def get(self):
@@ -42,8 +59,14 @@ class LedAPI(Resource):
         v = request.form['brightness']
         print 'value...', v
         v = int(v)
-        set_led(led_id, v)
+
+        # call set_led(led_id, v) but in a separate thread
+        h = threading.Thread(target=set_led, args=(led_id, v))
+        h.setDaemon(True)
+        h.start()
+
         return led_list[led_id]
+
 
 class ArmAPI(Resource):
     def get(self, arm_id):
@@ -64,12 +87,11 @@ api.add_resource(LedListAPI, '/leds')
 api.add_resource(ArmAPI, '/arms/<int:arm_id>')
 # api.add_resource(LedListAPI, '/leds')
 
+
 @app.route('/')
 def show_control():
     return render_template('control.html', led_list=led_list)
 
 
-
 if __name__ == '__main__':
-    print led_list
     app.run(debug=True, host='0.0.0.0')
