2fa218bf6d899507d60e9b11f281224201640047
PK Shiu <pk@imperial-consulting.com>
2014-10-21 21:37:44 -0400

    setup use package

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ long_description= 'A RESTful API server and a web server to control the PiGlow R
       keywords= 'Raspberry Pi PiGlow Flask Flask-restful Python RESTful',
       url= 'http://www.pkshiu.com',
 classifiers = classifiers,
-      py_modules= ['piglowserver'],
+packages = ['piglowserver'],
+#      py_modules= ['piglowserver'],
 # install_requires= ['rpi.gpio >= 0.5.4']
 )
